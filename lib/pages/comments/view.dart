2674c2344ca5f2cf1836b4f7022c976f3cb4d636
XtarNodde <onyekaejimns@gmail.com>
2023-03-15 15:29:18 +0100

    implement shader

--- lib/pages/comments/view.dart
+++ lib/pages/comments/view.dart
@@ -8,7 +8,9 @@ import 'logic.dart';
 
 class CommentsPage extends StatelessWidget {
   final logic = Get.put(CommentsLogic());
-  final state = Get.find<CommentsLogic>().state;
+  final state = Get
+      .find<CommentsLogic>()
+      .state;
   final PostsModel postsModel;
 
   CommentsPage({super.key, required this.postsModel});
@@ -16,109 +18,148 @@ class CommentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: Colors.black,title: Text("${postsModel.title}",style: TextStyle(color: Colors.white),),elevation: 0,),
+        appBar: AppBar(backgroundColor: Colors.black,
+          title: Text(
+            "${postsModel.title}", style: TextStyle(color: Colors.white),),
+          elevation: 0,),
         body: FutureBuilder<List<CommentsModel>>(
-          future: logic.getComments(id: postsModel.id.toString()),
-          builder: (context,snapshot) {
-            if (snapshot.hasData&& snapshot.data!.length>0) {
-              return ListView.builder(
-                itemBuilder: (context, index) => Wrap(
-                  children: [
-                    Wrap(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.all(8),
+            future: logic.getComments(id: postsModel.id.toString()),
+            builder: (context, snapshot) {
+              if (snapshot.hasData && snapshot.data!.length > 0) {
+                return ListView.builder(
+                  itemBuilder: (context, index) =>
+                      Wrap(
+                        children: [
+                          Wrap(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.all(8),
 
+                                width: Get.width,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    boxShadow: [
+                                      BoxShadow(color: Colors.black,
+                                          spreadRadius: 1,
+                                          blurStyle: BlurStyle.outer,
+                                          blurRadius: 4)
+                                    ],
+                                    borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(16),
+                                        topLeft: Radius.circular(16),
+                                        bottomRight: Radius.circular(16),
+                                        bottomLeft: Radius.circular(16))),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 4.0, right: 2),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          ShaderMask(shaderCallback: (bounds) {
+                                            return LinearGradient(colors: [
+                                              Colors.red,
+                                              Colors.white
+                                            ]).createShader(bounds);
+                                          },
+                                            child: Text(
+                                              " Name: ",
+                                              style: TextStyle(
+                                                  color: Colors.black87,
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              "${snapshot.data![index].name!}",
+                                              style: TextStyle(
+                                                  color: Colors.black54,
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            " Email: ",
+                                            style: TextStyle(
+                                                color: Colors.black87,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              "${snapshot.data![index].email!}",
+                                              style: TextStyle(
+                                                  color: Colors.black54,
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            " Body: ",
+                                            style: TextStyle(
+                                                color: Colors.black87,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              "${snapshot.data![index].body!}",
+                                              style: TextStyle(
+                                                  color: Colors.black54,
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                  itemCount: snapshot.data!.length,
+                );
+              }
+
+              else if (snapshot.connectionState == ConnectionState.waiting) {
+                return ListView.builder(
+                  itemBuilder: (context, index) =>
+                      Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          height: 100,
                           width: Get.width,
                           decoration: BoxDecoration(
-                              color: Colors.white,
-                              boxShadow: [BoxShadow(color: Colors.black,spreadRadius: 1,blurStyle: BlurStyle.outer,blurRadius: 4)],
-                              borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(16),topLeft:  Radius.circular(16),
-                                  bottomRight: Radius.circular(16),
-                                  bottomLeft: Radius.circular(16))),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 4.0,right: 2),
-                            child: Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      " Name: ",
-                                      style: TextStyle(color: Colors.black87,fontSize: 16,fontWeight: FontWeight.w600),
-                                    ),
-                                    Expanded(
-                                      child: Text(
-                                        "${snapshot.data![index].name!}",
-                                        style: TextStyle(color: Colors.black54,fontSize: 16,fontWeight: FontWeight.w600),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      " Email: ",
-                                      style: TextStyle(color: Colors.black87,fontSize: 16,fontWeight: FontWeight.w600),
-                                    ),
-                                    Expanded(
-                                      child: Text(
-                                        "${snapshot.data![index].email!}",  style: TextStyle(color: Colors.black54,fontSize: 16,fontWeight: FontWeight.w600),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      " Body: ",
-                                      style: TextStyle(color: Colors.black87,fontSize: 16,fontWeight: FontWeight.w600),
-                                    ),
-                                    Expanded(
-                                      child: Text(
-                                        "${snapshot.data![index].body!}",   style: TextStyle(color: Colors.black54,fontSize: 16,fontWeight: FontWeight.w600),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-
-                              ],
-                            ),
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(20),
                           ),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                itemCount: snapshot.data!.length,
-              );
-            }
-
-        else if(snapshot.connectionState==ConnectionState.waiting){
-              return ListView.builder(
-                itemBuilder: (context, index) =>
-                    Shimmer.fromColors(
-                      baseColor: Colors.grey.shade300,
-                      highlightColor: Colors.grey.shade100,
-                      child: Container(
-                        margin: EdgeInsets.all(10),
-                        height: 100,
-                        width: Get.width,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
                       ),
-                    ),
-                itemCount: 10,
-              );
+                  itemCount: 10,
+                );
+              }
+              else {
+                return Center(
+                  child: Icon(Icons.warning_amber_sharp,
+                      color: Colors.red, size: 40),
+                );
+              }
             }
-            else{
-              return Center(
-                child: Icon(Icons.warning_amber_sharp,
-                    color: Colors.red, size: 40),
-              );
-            } }
-     ));
+        ));
   }
 }
